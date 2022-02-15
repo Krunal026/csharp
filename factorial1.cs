98eced07c7f5b3bc30bdae0bf1a4936e3ecfdb7c
Krunal026 <95204344+Krunal026@users.noreply.github.com>
2022-02-15 13:28:46 +0530

    Add files via upload

---
+++ factorial1.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace fact
+{
+    class @factorial1
+    {
+        static void Factorial(int num)
+        {
+            int fact = 1;
+            while (num > 0)
+            {
+                fact = fact * num;
+                num--;
+            }
+
+            Console.WriteLine("fact =" + fact);
+
+        }
+
+        static void Main()
+        {
+            int num;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+                
+            Factorial(num);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
