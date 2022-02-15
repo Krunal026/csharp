c825a954ed285ff58cf5aee01a4d9682ce5fb620
Krunal026 <95204344+Krunal026@users.noreply.github.com>
2022-02-15 09:59:31 +0530

    Add files via upload

---
+++ reverse.cs
@@ -0,0 +1,42 @@
+﻿//to find reverse no. of 4 digit number...
+
+using System;
+
+namespace rev
+{
+    class @reverse
+    {
+        static void Main()
+        {
+            int num, num1, rev = 0, rem;
+            Console.WriteLine("Enter any 4 digit number");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entred 4 digit number is " +num);
+
+
+
+            num1 = num;
+
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+
+
+            Console.WriteLine("Reversed 4 digit number is " + rev);
+
+            Console.ReadLine();        }
+    }
+}
+
---
+++ swapping.cs
@@ -0,0 +1,34 @@
+﻿////wap to swap two numbers
+
+//using System;
+
+//namespace swap
+//{
+//    class @swapping
+//    {
+//        static void Main()
+//        {
+//            int a, b, c, d;
+//            Console.WriteLine("Enter 2 numbers for swapping");
+//            a = Convert.ToInt32(Console.ReadLine());
+//            b = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("a =" + a);
+//            Console.WriteLine("b =" + b);
+
+
+//            c = a;
+//            d = b;
+//            a = d;
+//            b = c;
+
+//            Console.WriteLine("After Swapping");
+
+//            Console.WriteLine("a =" + a);
+//            Console.WriteLine("b =" + b);
+
+//            Console.ReadLine();
+
+
+//        }
+//    }
+//}
