4ac7f983e023d0a4ab04422c2362c8add49a6fd7
Krunal026 <95204344+Krunal026@users.noreply.github.com>
2022-02-22 21:36:19 +0530

    Add files via upload

---
+++ hasa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace inheritance
+{
+    public class Address
+    {
+        public string addressline, state, city;
+
+        public Address(string addressline, string state, string city)
+        {
+            this.addressline = addressline;
+            this.state = state;
+            this.city = city;
+        }
+    }
+
+    public class Employee
+    {
+        public int id;
+        public string name;
+        public Address address;
+
+        public Employee(int id, string name, Address address)
+        {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+        }
+        public void display()
+        {
+            Console.WriteLine(id + " " + name + " " +
+                 address.addressline + " " + address.city + " " + address.state);
+        }
+    }
+
+    public class abc
+    {
+        static void Main()
+        {
+            Address a1 = new Address("G-13, Sec-3", "Noida", "UP");
+            Employee e1 = new Employee(1, "KRunal", a1);
+
+            e1.display();
+
+        }
+    }
+}
---
+++ hirarchical.cs
@@ -0,0 +1,82 @@
+﻿////hierarchical inheritance
+
+//using System;
+
+//namespace inheritance
+//{
+//    public class person
+//    {
+//        string name;
+//        string address;
+
+//        public void getpersondata(string name, string address)
+//        {
+//            this.name = name;
+//            this.address = address;
+//        }
+
+//        public void displayPersonData()
+//        {
+//            Console.WriteLine("name : " + name);
+//            Console.WriteLine("address : " + address);
+
+//        }
+
+//    }
+
+//    public class student : person
+//    {
+//        int rno;
+//        int marks;
+
+//        public void getstudentdata(int rno, int marks)
+//        {
+//            this.rno = rno;
+//            this.marks = marks;
+
+//        }
+//        public void displaystudentdata()
+//        {
+//            Console.WriteLine("Roll number :" + rno);
+//            Console.WriteLine("Marks :" + marks);
+//        }
+//    }
+
+//    public class employee : person
+//    {
+//        int salary;
+//        string designation;
+
+//        public void getemployeedata(int salary, string designation)
+//        {
+//            this.salary = salary;
+//            this.designation = designation;
+//        }
+
+//        public void displayemployeedata()
+//        {
+//            Console.WriteLine("Salary :" + salary);
+//            Console.WriteLine("Designation :" + designation);
+//        }
+//    }
+
+//    class abc
+//    {
+//        static void Main()
+//        {
+//            student s1 = new student();
+//            s1.getpersondata("Krunal", "Nagpur");
+//            s1.getstudentdata(26, 98);
+//            s1.displayPersonData();
+//            s1.displaystudentdata();
+
+//            employee e1 = new employee();
+//            e1.getpersondata("Vivek", "Pune");
+//            e1.getemployeedata(50000, "HR");
+//            e1.displayPersonData();
+//            e1.displayPersonData();
+//        }
+//    }
+
+
+//}
