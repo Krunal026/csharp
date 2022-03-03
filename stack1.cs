a2530394d91b41968d26eb382139c77b3f4a597b
Krunal026 <95204344+Krunal026@users.noreply.github.com>
2022-03-03 22:34:40 +0530

    Add files via upload

---
+++ stack1.cs
@@ -0,0 +1,51 @@
+﻿//using System;
+//using System.Collections;
+
+
+//namespace ConsoleApp7
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+
+//            Stack st = new Stack();
+
+//            st.Push("Amar");
+//            st.Push("Maria");
+//            st.Push("Gomes");
+//            st.Push("Wilson");
+
+
+//            Console.WriteLine("Current stack: ");
+//            foreach (String c in st)
+//            {
+//                Console.Write(c + " ");
+//            }
+//            Console.WriteLine();
+
+//            st.Push("Vinita");
+//            st.Push("Harshal");
+
+//            Console.WriteLine("The next poppable value in stack: {0}", st.Peek());
+
+//            Console.WriteLine("Current stack: ");
+//            foreach (String c in st)
+//            {
+//                Console.Write(c + " ");
+//            }
+//            Console.WriteLine();
+
+//            Console.WriteLine("The next poppable value in stack: {0}", st.Pop());
+
+//            Console.WriteLine("Current stack: ");
+//            foreach (String c in st)
+//            {
+//                Console.Write(c + " ");
+//            }
+//            Console.WriteLine();
+
+
+//        }
+//    }
+//}
